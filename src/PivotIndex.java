337798eddde4c72859dc9cd3b5ee9bcc33b54488
Baisakhi Sahoo <sahoobaisakhi182@gmail.com>
2026-08-29 11:52:52 +0530

    Added Pivot Index Solution

---
+++ src/PivotIndex.java
@@ -0,0 +1,22 @@
+public class PivotIndex {
+    static void main() {
+        int[] nums ={1, 7, 3, 6, 5, 6};
+        System.out.println( pivotIndex(nums));
+    }
+    public static int pivotIndex(int[] nums) {
+        int totalsum=0;
+        for(int sum:nums){
+            totalsum+=sum;
+        }
+        int leftsum=0;
+        for(int i=0;i<nums.length;i++){
+            int rightsum=totalsum-leftsum-nums[i];
+            if(leftsum==rightsum){
+                return i;
+            }
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+}
+
